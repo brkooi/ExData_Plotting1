9a00a9ef4685510ec65aeb03d717de0ad3f41e81
Ben Kooi <brkooi@hotmail.com>
2020-10-18 22:17:49 +0200

    Commit 2

---
+++ plot2.R
@@ -0,0 +1,44 @@
+# Data science Course 4 - Exploratory Data Analysis
+# Week 1 -Assignment
+# Script plot2.R
+
+
+#load packages
+packages <- c("data.table","dplyr")
+sapply(packages, require, character.only=TRUE, quietly=TRUE)
+
+# download file and unzip datafiles
+path_files<-getwd()
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(url, "household_power_consumption.zip")
+unzip(zipfile = "household_power_consumption.zip")
+
+# read unzipped file into table
+hhpc<-fread("household_power_consumption.txt")
+
+# filter the data on the dates to analyze
+hhpc<-hhpc  %>% filter(Date=="1/2/2007" | Date=="2/2/2007")
+
+# transform the variables to the neccesary data-types
+hhpc$Global_active_power<-as.numeric(hhpc$Global_active_power)
+hhpc$Global_reactive_power<-as.numeric(hhpc$Global_reactive_power)
+hhpc$Voltage<-as.numeric(hhpc$Voltage)
+hhpc$Global_intensity<-as.numeric(hhpc$Global_intensity)
+hhpc$Sub_metering_1<-as.numeric(hhpc$Sub_metering_1)
+hhpc$Sub_metering_2<-as.numeric(hhpc$Sub_metering_2)
+hhpc$Sub_metering_3 <-as.numeric(hhpc$Sub_metering_3)
+
+# concatenate Date and Time to DateTime
+hhpc$DateTime <- as.POSIXct(paste(hhpc$Date, hhpc$Time), format="%d/%m/%Y %H:%M:%S")
+
+# open a PNG-device
+png(filename = "plot2.png", width = 480, height = 480, units = "px")
+
+# plot the line-diagram
+plot(hhpc$DateTime, hhpc$Global_active_power,type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+
+# close the device
+dev.off()
+
+
